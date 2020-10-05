2132d0891e62f8330140c47b49397fbc463732f8
Alan Young <alanryoung01@gmail.com>
2020-10-05 19:14:26 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,7 +61,7 @@ def sell_pets
 end
 
 def list_pets
-  "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s). "
+  "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
 end
 
 
